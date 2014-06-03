d5b5868b261d30d335b1da9a0c3bf87bab7212a3
the Phoeron <thephoeron@me.com>
2014-06-03 09:28:07 -0400

    Move footer to template function, add to basic and standard templates, clean-up css

--- templates.lisp
+++ templates.lisp
@@ -6,6 +6,27 @@
 
 (in-package :llthw)
 
+(defun llthw-footer ()
+  (cl-who:with-html-output (hunchentoot::*standard-output*)
+    (:div :id "footer"
+      (:div :class "container"
+        (:p :align "center"
+          (:a :href "http://common-lisp.net/" :alt "Crafted in Common Lisp" :title "Crafted in Common Lisp" "(λ)") (:br)
+          "Copyright &copy; 2012 &ndash; 2014, \"the Phoeron\" Colin J.E. Lupton. All Rights Reserved." (:br)
+          "Powered by "
+          (:a :href "http://www.webfaction.com/?affiliate=thephoeron" :target "_blank" "Webfaction") ", "
+          (:a :href "http://www.sbcl.org/" :target "_blank" "SBCL") ", "
+          (:a :href "https://github.com/davazp/jscl" :target "_blank" "JSCL") ", "
+          (:a :href "http://www.quicklisp.org/" :target "_blank" "Quicklisp") ", "
+          (:a :href "http://weitz.de/hunchentoot/" :target "_blank" "Hunchentoot") ", "
+          (:a :href "https://github.com/Inaimathi/cl-css" :target "_blank" "CL-CSS") ", "
+          (:a :href "http://weitz.de/cl-who/" :target "_blank" "CL-WHO") ", "
+          (:a :href "https://github.com/vsedach/Parenscript" :target "_blank" "Parenscript") ", "
+          (:a :href "https://github.com/3b/3bmd" :target "_blank" "3bmd") ", "
+          (:a :href "https://github.com/redline6561/colorize" :target "_blank" "Colorize") ", "
+          (:a :href "http://jquery.com/" :target "_blank" "jQuery") ", and "
+          (:a :href "http://getbootstrap.com/" :target "_blank" "Bootstrap") ".")))))
+
 (defmacro basic-llthw-page ((&key (title "L(λ)THW")) &body body)
   `(cl-who:with-html-output-to-string (hunchentoot::*standard-output* nil :prologue t :indent t)
     (:html :lang "en"
@@ -25,6 +46,7 @@
               (:li (:a :href "/book/" "Book"))
               (:li (:a :href "/resources/" "Resources")))))
         ,@body
+        (llthw-footer)
         (:script :src "/static/js/jquery.js" :type "text/javascript")
         (:script :src "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js")))))
 
@@ -54,6 +76,7 @@
               ,@body)
             (:div :class "col-md-4"
               (:div :id "console" :data-spy "affix" :data-offset-top "60" :class "affix"))))
+        (llthw-footer)
         (:script :src "/static/js/jquery.js" :type "text/javascript")
         (:script :src "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js")
         (:script :src "/static/js/jscl.js" :type "text/javascript")
