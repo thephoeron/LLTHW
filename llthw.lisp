6ef119106d52d04228959bd80fe32916a0273f8c
the Phoeron <thephoeron@me.com>
2015-01-09 12:16:07 -0500

    update copyright notice in source files

--- llthw.lisp
+++ llthw.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: LLTHW; Base: 10 -*-
 ;;;; file: llthw.lisp
 
-;;;; Copyright (c) 2012--2014 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; Copyright (c) 2012--2015 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
 
 (in-package :llthw)
