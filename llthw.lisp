d532504f5f26692c6444a540f6824f32d4ccfc24
the Phoeron <thephoeron@users.noreply.github.com>
2015-05-11 00:34:59 -0400

    Update llthw.lisp
    minor stylistic changes to code layout

--- llthw.lisp
+++ llthw.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: LLTHW; Base: 10 -*-
 ;;;; file: llthw.lisp
 
-;;;; Copyright (c) 2012--2015 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; Copyright (c) 2012--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
 
 (in-package :llthw)
@@ -9,7 +9,11 @@
 (defun llthw-start (&key (port 8080))
   "Start LLTHW server."
   (when (null *acc*)
-    (setf *acc* (make-instance 'hunchentoot:easy-acceptor :port port :access-log-destination *acc-log* :message-log-destination *msg-log*)))
+    (setf *acc* 
+          (make-instance 'hunchentoot:easy-acceptor 
+                         :port port
+                         :access-log-destination *acc-log*
+                         :message-log-destination *msg-log*)))
   (hunchentoot:start *acc*))
 
 (defun llthw-stop ()
@@ -19,8 +23,9 @@
   (setf *acc* nil))
 
 ;; Assumes SBCL + Quicklisp
+
 (defun llthw-restart (&key (port 8080))
-  "Stop, reload/recompile, and start the LLTHW server again.  Also rescans the book, reference, and try-lisp directories for new files."
+  "Stop, reload/recompile, and start the LLTHW server again."
   (llthw-stop)
   (ql:quickload "llthw")
   (sb-ext:gc :full t)
